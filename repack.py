f2751d4df6a15fbdea53c6674e402732fbd0e9a0
Matthew Ruffalo <matthew.ruffalo@case.edu>
2011-11-23 15:46:58 -0500

    Run repack if not pretend

--- repack.py
+++ repack.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+from os.path import split as osps
 from subprocess import Popen, PIPE
 
 git_command = ('git', '--git-dir={}')
@@ -8,12 +9,14 @@ repack = git_command + ('repack', '-Ad')
 
 p = argparse.ArgumentParser()
 p.add_argument('git_dir_path', nargs='+')
+p.add_argument('--pretend', action='store_true')
 args = p.parse_args()
 
 for git_dir_path in args.git_dir_path:
     s = Popen((x.format(git_dir_path) for x in count_objects), stdout=PIPE)
     s.wait()
     output = s.stdout.read()
-    print(repr(output))
     object_count = int(output.split()[0])
-    print(object_count)
+    print('{}: {} loose objects'.format(osps(git_dir_path)[0], object_count))
+    if object_count and not args.pretend:
+        Popen((x.format(git_dir_path) for x in repack)).wait()
